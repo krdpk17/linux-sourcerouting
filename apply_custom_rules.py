4b0b79443df84f3b89ad611d4b41e0f6d125ab6a
krdpk17 <deepak.kumar@citrix.com>
2020-05-12 09:40:17 +0530

    initial commit

---
+++ apply_custom_rules.py
@@ -0,0 +1,15 @@
+import pdb
+import pyroute2
+class RouteManager:
+
+    def __init__(exclusion_filter=[0, 253, 254, 255]):
+        self.exclusion_filter = exclusion_filter
+    def get_routes_from_rules():
+        iproute = pyroute2.IPRoute()
+        routes = iproute.get_routes()
+        pdb.set_trace()
+        filtered_routes = [route for route in routes if route['attrs']['RTA_TABLE'] not in self.exclusion_filter]
+        print("{}".format(filtered_routes))
+
+routeManager = RouteManager()
+routeManager.get_routes_from_rules()
