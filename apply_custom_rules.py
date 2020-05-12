90afb3cbb9d87f87dcfa2bd10c565332c6909c00
krdpk17 <deepak.kumar@citrix.com>
2020-05-12 12:16:08 +0530

    Update apply_custom_rules.py

--- apply_custom_rules.py
+++ apply_custom_rules.py
@@ -5,7 +5,7 @@ class RouteManager:
     def __init__(self, exclusion_filter=[0, 253, 254, 255]):
         self.exclusion_filter = exclusion_filter
         self.ip_rules = {}
-        self.ip_routes = {}
+        self.ip_routes = []
         self.routes_by_priority = {}
     
     def parse_rule(self, rule):
@@ -13,19 +13,26 @@ class RouteManager:
         attr_dict = {attr[0]:attr[1] for attr in rule_attrs}
         return attr_dict
 
+    def parse_route(self, route):
+        table_id = route['table']
+        pdb.set_trace()
+        if table_id not in self.ip_rules:
+            print("Couldn't find ip rule priority for {} table id and {} route".format(table_id, route))
+            return
+        priority = self.ip_rules['table_id']
+        return {priority: route}
 
     def fetch_rules(self):
         iproute = pyroute2.IPRoute()
         rules = iproute.get_rules()
         self.ip_rules = {rule['table']:(lambda rule: self.parse_rule(rule))(rule) for rule in rules if rule['table'] not in self.exclusion_filter}
-        pdb.set_trace()
         print("Found {} rules".format(len(self.ip_rules)))
         return
 
     def fetch_routes(self):
         iproute = pyroute2.IPRoute()
         routes = iproute.get_routes()
-        self.ip_routes = [route for route in routes if route['table'] not in self.exclusion_filter]
+        self.ip_routes = [(lambda route: self.parse_route(rule))(route)  for route in routes if route['table'] not in self.exclusion_filter]
         print("Found {} routes".format(len(self.ip_routes)))
         return
     
