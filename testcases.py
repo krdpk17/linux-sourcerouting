7e1d486c694350851e6924a03747dac93c992408
krdpk17 <deepak.kumar@citrix.com>
2020-05-12 13:19:16 +0530

    Update testcases.py

--- testcases.py
+++ testcases.py
@@ -3,16 +3,28 @@ import pyroute2
 
 class Tests:
     def __init__(self):
-        self.table_id = 100
-    def create_rule_add_routes(self):
+        self.table_id_single_rule = 100
+        self.table_id_multiple_rule = 200
+    def create_single_rule_multiple_routes(self):
+        iproute = pyroute2.IPRoute()
+        #Add rule
+        iproute.rule('add', self.table_id_single_rule, 32000, src='1.1.1.1')
+        #Add routes
+        iproute.route("add", dst="10.0.0.0", mask=24, gateway="172.17.0.3", table=self.table_id_single_rule)
+        iproute.route("add", dst="11.0.0.0", mask=24, gateway="172.17.0.3", table=self.table_id_single_rule)
+        iproute.route("add", dst="0.0.0.0", mask=24, gateway="172.17.0.3", table=self.table_id_single_rule)
+    def create_multi_rule_multiple_routes(self):
         iproute = pyroute2.IPRoute()
         pdb.set_trace()
-        #Add rule
-        iproute.rule('add', self.table_id, 32000, src='1.1.1.1')
+        #Add rules with same table ID
+        iproute.rule('add', self.table_id_multiple_rule, 32000, src='1.1.1.1')
+        iproute.rule('add', self.table_id_multiple_rule, 32001, src='2.1.1.1')
+        iproute.rule('add', self.table_id_multiple_rule, 32002, dst='2.1.1.1')
+        iproute.rule('add', self.table_id_multiple_rule, 32002, src='3.1.1.1', dst='4.1.1.1')
         #Add routes
-        iproute.route("add", dst="10.0.0.0", mask=24, gateway="172.17.0.3", table=self.table_id)
-        iproute.route("add", dst="11.0.0.0", mask=24, gateway="172.17.0.3", table=self.table_id)
-        iproute.route("add", dst="0.0.0.0", mask=24, gateway="172.17.0.3", table=self.table_id)
+        iproute.route("add", dst="10.0.0.0", mask=24, gateway="172.17.0.3", table=self.table_id_multiple_rule)
+        iproute.route("add", dst="11.0.0.0", mask=24, gateway="172.17.0.3", table=self.table_id_multiple_rule)
+        iproute.route("add", dst="0.0.0.0", mask=24, gateway="172.17.0.3", table=self.table_id_multiple_rule)
     def delete_rules():
         iproute = pyroute2.IPRoute()
         pdb.set_trace()
@@ -22,7 +34,8 @@ class Tests:
         self.delete_rules()
 
     def execute(self):
-        self.create_rule_add_routes()
+        self.create_single_rule_multiple_routes()
+        self.create_multi_rule_multiple_routes()
 
 tests = Tests()
 tests.execute()
