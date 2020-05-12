69861e62c62d0e68465960bf21c03b7a6bcb7ed6
krdpk17 <deepak.kumar@citrix.com>
2020-05-12 09:41:36 +0530

    Update apply_custom_rules.py

--- apply_custom_rules.py
+++ apply_custom_rules.py
@@ -2,9 +2,9 @@ import pdb
 import pyroute2
 class RouteManager:
 
-    def __init__(exclusion_filter=[0, 253, 254, 255]):
+    def __init__(self, exclusion_filter=[0, 253, 254, 255]):
         self.exclusion_filter = exclusion_filter
-    def get_routes_from_rules():
+    def get_routes_from_rules(self):
         iproute = pyroute2.IPRoute()
         routes = iproute.get_routes()
         pdb.set_trace()
