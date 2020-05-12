40edd6ebfe918ec067fe9334920d10e2fac448b6
krdpk17 <deepak.kumar@citrix.com>
2020-05-12 10:56:20 +0530

    Update apply_custom_rules.py

--- apply_custom_rules.py
+++ apply_custom_rules.py
@@ -5,21 +5,26 @@ class RouteManager:
     def __init__(self, exclusion_filter=[0, 253, 254, 255]):
         self.exclusion_filter = exclusion_filter
         self.ip_rules = {}
-    def get_rules(self):
+        self.ip_routes = {}
+
+    def fetch_rules(self):
         iproute = pyroute2.IPRoute()
         pdb.set_trace()
         rules = iproute.get_rules()
         self.ip_rules = [rule for rule in rules if rule['table'] not in self.exclusion_filter]
+        print("Found {} rules".format(len(self.ip_rules)))
         return
-    def get_routes(self):
+
+    def fetch_routes(self):
         iproute = pyroute2.IPRoute()
         routes = iproute.get_routes()
         pdb.set_trace()
-        filtered_routes = [route for route in routes if route['table'] not in self.exclusion_filter]
-        print("{}".format(filtered_routes))
+        self.ip_routes = [route for route in routes if route['table'] not in self.exclusion_filter]
+        print("Found {} routes".format(len(self.ip_routes)))
+        return
 
     def process_custom_rules(self):
-        ip_rules = self.get_rules()
-        ip_routes = self.get_routes()
+        self.fetch_rules()
+        self.fetch_routes()
 routeManager = RouteManager()
 routeManager.process_custom_rules()
