3cc82554394e52241a70006ee81e00cf79f2b4ea
krdpk17 <deepak.kumar@citrix.com>
2020-05-12 10:16:17 +0530

    Update testcases.py

--- testcases.py
+++ testcases.py
@@ -10,7 +10,7 @@ class Tests:
         #Add rule
         iproute.rule('add', self.table_id, 32000, src='1.1.1.1')
         #Add routes
-        ip.route("add", dst="10.0.0.0", mask=24, gateway="192.168.0.1", table=self.table_id)
+        iproute.route("add", dst="10.0.0.0", mask=24, gateway="192.168.0.1", table=self.table_id)
     def delete_rules():
         iproute = pyroute2.IPRoute()
         pdb.set_trace()
