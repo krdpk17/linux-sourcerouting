27d6cf2952888296828b71a02e1d5e902b629b63
krdpk17 <deepak.kumar@citrix.com>
2020-05-13 10:42:28 +0530

    Update apply_custom_rules.py

--- apply_custom_rules.py
+++ apply_custom_rules.py
@@ -75,7 +75,7 @@ class RouteManager:
             if priority not in self.routes_by_priority:
                 routes_by_priority[priority] = []
             routes_by_priority[priority].append(route[1])
-        routes_by_priority = OrderedDict(sorted(routes_by_priority.items()))
+        self.routes_by_priority = OrderedDict(sorted(routes_by_priority.items()))
         print("Number of entries after priority based merge is {}".format(len(routes_by_priority)))
         return
 
